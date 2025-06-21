c424c961ef70835967ea067d34ec87235afa814b
Yulchikss <yuliapelia19@gmail.com>
2025-06-21 19:35:04 +0300

    added tracking app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,66 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { GlobalStyles } from "@/constants/styles";
+import AllExpenses from "@/screens/AllExpenses";
+import ManageExpenses from "@/screens/ManageExpense";
+import RecentExpenses from "@/screens/RecentExpenses";
+import IconButton from "@/ui/IconButton";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const Stack = createNativeStackNavigator();
+const BottomTabsNavigator = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+    <BottomTabsNavigator.Navigator screenOptions={{ 
+      headerStyle: {
+        backgroundColor: GlobalStyles.colors.primary500,
+      },
+      headerTintColor: '#fff',
+      tabBarStyle: {
+        backgroundColor: GlobalStyles.colors.primary500,
+      },
+      headerRight: () => (
+        <IconButton icon="add" size={24} color="white" onPress={() => {}} />
+      ),
+     }}>
+      <BottomTabsNavigator.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          title: 'Recent Expenses',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="hourglass-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <BottomTabsNavigator.Screen name="AllExpenses" component={AllExpenses} options={{
+        title: 'All Expenses',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="calendar-outline" color={color} size={size} />
+        ),
+      }} />
+    </BottomTabsNavigator.Navigator>
+  );
+}
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <>
       <StatusBar style="auto" />
-    </ThemeProvider>
+      {/* <NavigationContainer> */}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="ExpensesOverview"
+          component={ExpensesOverview}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
+      </Stack.Navigator>
+      {/* </NavigationContainer> */}
+    </>
   );
 }
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,7 @@
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+
+function RecentExpenses() {
+  return <ExpensesOutput periodName="Last 7 Days" />;
+}
+
+export default RecentExpenses;
